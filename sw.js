03d06f49a8fc651c458d8b2d3a3ac189034c9fb9
Francisco <[a02103012@utmir.edu.mx]>
2024-11-19 12:57:31 -0600

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -4,17 +4,17 @@ importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
-const INMUTABLE_CACHE = 'inmutable-v1'; // Corregido de 'inmitable-v1'
+const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-   // '/',
-    'index.html', // Corregido de 'index.thml'
+// '/',
+    'index.html',
     'css/style.css',
     'img/favicon.ico',
     'img/avatars/hulk.jpg',
     'img/avatars/spiderman.jpg',
     'img/avatars/thor.jpg',
-    'img/avatars/wolverine.jpg', // Corregido de 'img/avatars//wolverine.jpg'
+    'img/avatars/wolverine.jpg',
     'js/app.js',
     'js/sw-utils.js'
 ];
